3edd692bcae3e1cfbd1da95273231b37377bc643
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-02-22 10:07:51 +0100

    Create a database named vet_clinic and Create a table named animals

--- schema.sql
+++ schema.sql
@@ -1,5 +1,15 @@
 /* Database schema to keep the structure of entire database. */
-
+DROP TABLE IF EXISTS animals;
 CREATE TABLE animals (
-    name varchar(100)
+    id INT PRIMARY KEY NOT NULL,
+    name varchar(100) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL
 );
+--           List of relations
+--  Schema |  Name   | Type  |  Owner   
+-- --------+---------+-------+----------
+--  public | animals | table | postgres
+-- (1 row)
