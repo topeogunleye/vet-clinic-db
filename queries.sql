9d2958d7c13ca5b9d3899b61b40c71438c587708
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-02-22 21:23:07 +0100

    Verify if all records in animals table still exist

--- queries.sql
+++ queries.sql
@@ -147,3 +147,20 @@ TRUNCATE TABLE animals;
 ROLLBACK;
 -- ROLLBACK
 
+-- After the roll back verify if all records in the animals table still exist
+-- SELECT * FROM animals;
+--  id |    name    | date_of_birth | escape_attempts | neutered | weight_kg | species 
+-- ----+------------+---------------+-----------------+----------+-----------+---------
+--   1 | Agumon     | 2020-02-03    |               0 | t        |     10.23 | digimon
+--   2 | Gabumon    | 2018-11-15    |               2 | t        |         8 | digimon
+--   4 | Devimon    | 2017-05-12    |               5 | t        |        11 | digimon
+--   6 | Plantmon   | 2022-11-15    |               2 | t        |      -5.7 | digimon
+--   8 | Angemon    | 2005-06-12    |               1 | t        |       -45 | digimon
+--   9 | Boarmon    | 2005-06-07    |               7 | t        |      20.4 | digimon
+--   3 | Pikachu    | 2021-01-07    |               1 | f        |     15.04 | pokemon
+--   5 | Charmander | 2020-02-08    |               0 | f        |       -11 | pokemon
+--   7 | Squirtle   | 1993-04-02    |               3 | f        |    -12.13 | pokemon
+--  10 | Blossom    | 1998-10-13    |               3 | t        |        17 | pokemon
+-- (10 rows)
+
+
