7f5b23458fef7831b6c83147a0ac38a8ec249a1d
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-02-25 20:09:42 +0100

    Select who was the last animal seen by William Tatcher

--- queries.sql
+++ queries.sql
@@ -349,3 +349,40 @@ GROUP BY
   owners.full_name
 ORDER BY
   COUNT(*) DESC LIMIT 1;
+
+
+--  Write queries to answer the following:
+-- Who was the last animal seen by William Tatcher?
+-- How many different animals did Stephanie Mendez see?
+-- List all vets and their specialties, including vets with no specialties.
+-- List all animals that visited Stephanie Mendez between April 1st and August 30th, 2020.
+-- What animal has the most visits to vets?
+-- Who was Maisy Smith's first visit?
+-- Details for most recent visit: animal information, vet information, and date of visit.
+-- How many visits were with a vet that did not specialize in that animal's species?
+-- What specialty should Maisy Smith consider getting? Look for the species she gets the most.
+
+-- Who was the last animal seen by William Tatcher?
+SELECT 
+  owners.full_name
+FROM  
+  owners 
+JOIN
+  animals 
+ON
+  owners.id = animals.owner_id
+JOIN
+  visits
+ON
+  animals.id = visits.animal_id
+JOIN
+  vets
+ON
+  visits.vet_id = vets.id
+WHERE
+  vets.name = 'William Tatcher'
+ORDER BY
+  visits.date_of_visit DESC
+LIMIT 1;  
+
+
