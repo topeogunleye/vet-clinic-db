fd7ff684f4ea5c8464894ae782ac5062c313a243
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-02-25 22:11:56 +0100

    Change animal name from Agumon to Blossom

--- data.sql
+++ data.sql
@@ -146,5 +146,5 @@ INSERT INTO visits (animal_id, vet_id, date_of_visit) VALUES
 ((SELECT id FROM animals WHERE name = 'Boarmon'), (SELECT id FROM vets WHERE name = 'Vet Maisy Smith'), '2020-02-27'),
 ((SELECT id FROM animals WHERE name = 'Boarmon'), (SELECT id FROM vets WHERE name = 'Vet Maisy Smith'), '2020-08-03'),
 ((SELECT id FROM animals WHERE name = 'Blossom'), (SELECT id FROM vets WHERE name = 'Vet Stephanie Mendez'), '2020-05-24'),
-((SELECT id FROM animals WHERE name = 'Agumon'), (SELECT id FROM vets WHERE name = 'Vet William Tatcher'), '2021-01-11');
+((SELECT id FROM animals WHERE name = 'Blossom'), (SELECT id FROM vets WHERE name = 'Vet William Tatcher'), '2021-01-11');
 
