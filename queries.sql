f0eaee7b9997d7e7ad5abca6d7f531fd7bfc8e66
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-02-25 21:08:56 +0100

    List all animals that visited Stephanie Mendez between April 1st and August 30th, 2020

--- queries.sql
+++ queries.sql
@@ -414,3 +414,20 @@ JOIN
   species
 ON
   specializations.species_id = species.id;
+
+-- List all animals that visited Stephanie Mendez between April 1st and August 30th, 2020
+
+SELECT 
+  animals.name 
+FROM
+  animals
+JOIN
+  visits
+ON
+  animals.id = visits.animal_id
+JOIN
+  vets
+ON
+  visits.vet_id = vets.id
+WHERE
+  vets.name = 'Stephanie Mendez' AND visits.date_of_visit BETWEEN '2020-04-01' AND '2020-08-30';
