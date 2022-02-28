a4bc62075b74e4c8f46573bde10056f339fb8bca
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-02-28 16:05:38 +0100

    Add Schema.sql

--- schema.sql
+++ schema.sql
@@ -1,41 +1,3 @@
-/* Database schema to keep the structure of entire database. */
-DROP TABLE IF EXISTS animals;
-CREATE TABLE animals (
-    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
-    name varchar(100) NOT NULL,
-    date_of_birth DATE NOT NULL,
-    escape_attempts INT,
-    neutered BOOLEAN,
-    weight_kg DECIMAL
-);
---           List of relations
---  Schema |  Name   | Type  |  Owner   
--- --------+---------+-------+----------
---  public | animals | table | postgres
--- (1 row)
-
-ALTER TABLE animals  
-ADD species varchar(150);
--- ALTER TABLE
--- \d animals
---                           Table "public.animals"
---      Column      |          Type          | Collation | Nullable | Default 
--- -----------------+------------------------+-----------+----------+---------
---  id              | integer                |           | not null | 
---  name            | character varying(100) |           | not null | 
---  date_of_birth   | date                   |           | not null | 
---  escape_attempts | integer                |           |          | 
---  neutered        | boolean                |           |          | 
---  weight_kg       | numeric                |           |          | 
---  species         | character varying(255) |           |          | 
--- Indexes:
---     "animals_pkey" PRIMARY KEY, btree (id)
-
--- Create a table named owners with the following columns:
--- id: integer (set it as autoincremented PRIMARY KEY)
--- full_name: string
--- age: integer
-DROP TABLE IF EXISTS owners;
 CREATE TABLE owners(
   id INT GENERATED ALWAYS AS IDENTITY,
   full_name VARCHAR(20),
@@ -43,64 +5,59 @@ CREATE TABLE owners(
   PRIMARY KEY(id)
 );
 
--- Create a table named species with the following columns:
--- id: integer (set it as autoincremented PRIMARY KEY)
--- name: string
-DROP TABLE IF EXISTS species;
-CREATE TABLE species (
-    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
-    name varchar(100) NOT NULL
+CREATE TABLE species(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(20),
+  PRIMARY KEY(id)
 );
 
--- Modify animals table:
--- Make sure that id is set as autoincremented PRIMARY KEY
--- Remove column species
--- Add column species_id which is a foreign key referencing species table
--- Add column owner_id which is a foreign key referencing the owners table
-
-ALTER TABLE animals
-DROP COLUMN species;
-ALTER TABLE animals
-ADD COLUMN species_id INT REFERENCES species(id);
-ALTER TABLE animals
-ADD COLUMN owner_id INT REFERENCES owners(id);
-
--- Create a table named vets with the following columns:
--- id: integer (set it as autoincremented PRIMARY KEY)
--- name: string
--- age: integer
--- date_of_graduation: date
-
-DROP TABLE IF EXISTS vets;
-CREATE TABLE vets (
-    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
-    name varchar(100) NOT NULL,
-    age INT NOT NULL,
-    date_of_graduation DATE NOT NULL   
+CREATE TABLE animals(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(20),
+  date_of_birth DATE,
+  escape_attempts INT,
+  neutered BOOLEAN,
+  weight_kg DECIMAL,
+  species_id INT REFERENCES species(id),
+  owner_id INT REFERENCES owners(id),
+  PRIMARY KEY(id)
 );
 
--- There is a many-to-many relationship between the tables species and vets: a vet can specialize in multiple species, and a species can have multiple vets specialized in it. Create a "join table" called specializations to handle this relationship.
-
-DROP TABLE IF EXISTS specializations;
-CREATE TABLE specializations (
-    vet_id INT REFERENCES vets(id),
-    species_id INT REFERENCES species(id)
+CREATE TABLE vets(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(20),
+  age INT,
+  date_of_graduation DATE,
+  PRIMARY KEY(id)
 );
--- There is a many-to-many relationship between the tables animals and vets: an animal can visit multiple vets and one vet can be visited by multiple animals. Create a "join table" called visits to handle this relationship, it should also keep track of the date of the visit.
 
-DROP TABLE IF EXISTS visits;
-CREATE TABLE visits (
-    animal_id INT REFERENCES animals(id),
-    vet_id INT REFERENCES vets(id),
-    date_of_visit DATE NOT NULL
+CREATE TABLE specializations(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  species_id INT REFERENCES species(id),
+  vet_id INT REFERENCES vets(id),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE visits(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  animal_id INT REFERENCES animals(id),
+  vet_id INT REFERENCES vets(id),
+  date_of_visit DATE,
+  PRIMARY KEY(id)
 );
 
 -- Add an email column to your owners table
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 
+-- -- Optimizations
+-- Drop Column Vet ID
+-- ALTER TABLE visits 
+-- DROP COLUMN vet_id;
 
--- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
-INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+-- CREATE TABLE vet_id (
+--     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+--     vet_id INT REFERENCES vet(id)
+-- );
 
--- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
-insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
+
+CREATE INDEX animal_visit ON visits(animal_id ASC);
